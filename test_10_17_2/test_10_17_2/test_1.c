569d05763782c80cd00fb392ba44dc1ce9718e4e
blackcurrent <blackcurrent@foxmail.com>
2025-10-19 16:11:06 +0800

    第三次上传

---
+++ test_10_17_2/test_10_17_2/test_1.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	char w = 0;
+//	scanf("%c", &w);
+//	printf("%c", w);
+//	return 0;
+//
+#include<stdio.h>
+int main()
+{
+	char ch[] = {'a','b','c','d'};
+	char ch2[] = "abcde";
+	printf("%s\n", ch);
+	printf("%s\n", ch2);
+	return 0;
+}
