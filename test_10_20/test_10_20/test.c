7063117957b2894bbb3e89e53af52e2846e51f22
blackcurrent <blackcurrent@foxmail.com>
2025-10-21 01:37:21 +0800

    第五次上传

---
+++ test_10_20/test_10_20/test.c
@@ -0,0 +1,77 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	int arr[3] = { 0,1 };
+//	printf("%d\n", arr[2]);
+//	return 0;
+//}
+#include<stdio.h>
+//int main()
+//{
+//	int arr[3];
+//	printf("%d\n", arr[1]);
+//	return 0;
+//}
+//Max(int x, int y)
+//{
+//	return (x > y ? x : y);
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	printf("%d\n", Max(a, b));
+//	return 0;
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int m = n / 12 * 4 + 2;
+//	printf("%d", m);
+//	return 0;
+//}
+//int main()
+//{
+//	int ret=printf("Hello World!");
+//	printf("\n%d\n", ret);
+//	return 0;
+//}
+//int main() 
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	while (scanf("%d %d %d", &a, &b, &c))
+//	{
+//		if (a + b > c && a + c > b && b + c > a)
+//		{
+//			if (a == b && b == c)
+//			{
+//				printf("EQ TR\n");
+//			}
+//			else if (a == b | b == c | a == c)
+//			{
+//				printf("SS TR\n");
+//			}
+//			else {
+//				printf("Ori TR\n");
+//			}
+//		}
+//		else {
+//			printf("not a triangle.\n");
+//		}
+//	}
+//	
+//	return 0;
+//}
+//int main()
+//{
+//	double L = 0;
+//	double M = 0;
+//	scanf("%lf %lf", &L, &M);
+//	printf("%lf", M / L/L);
+//	return 0;
+//}
